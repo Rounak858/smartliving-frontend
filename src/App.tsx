12691ae48408bf08b661965e3c9a9a3f1cf99e02
Rounak858 <91734046+Rounak858@users.noreply.github.com>
2025-06-18 01:35:56 +0530

    frist commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import SmartLivingDashboard from './components/SmartLivingDashboard';
+import './index.css'; // Make sure your Tailwind CSS is imported
+
+function App() {
+  return (
+    <div className="App">
+      <SmartLivingDashboard />
+    </div>
+  );
+}
+
+export default App;
